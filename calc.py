2e562c90c402ce9a07680e5bfd6d2ea092d18a84
MaltaWebDev <MaltaWebDev@gmail.com>
2023-03-05 19:54:45 +0100

    Complete calculations

--- calc.py
+++ calc.py
@@ -6,72 +6,69 @@ while True:
 
     oper = input("\nChoose an option from the menu: ")
 
-    # addition
+    # Addition
     if oper == "0":
         value_one = float(input("\nFirst value:"))
         value_two = float(input("\nSecond value:"))
         print(f"The answer is: {value_one + value_two}")
         break
 
-    # subtration
+    # Subtration
     elif oper == "1":
         value_one = float(input("\nFirst value:"))
         value_two = float(input("\nSecond value:"))
         print(f"The answer is: {value_one - value_two}")
         break
 
-    # multiplication
+    # Multiplication
     elif oper == "2":
         value_one = float(input("\nFirst value:"))
         value_two = float(input("\nSecond value:"))
         print(f"The answer is: {value_one * value_two}")
         break
 
-    # division
+    # Division
     elif oper == "3":
         value_one = float(input("\nFirst value:"))
         value_two = float(input("\nSecond value:"))
         print(f"The answer is: {value_one / value_two}")
         break
 
-    # modulo
+    # Modulo
     elif oper == "4":
         value_one = float(input("\nFirst value:"))
         value_two = float(input("\nSecond value:"))
         print(f"The answer is: {value_one % value_two}")
         break
         
-    # raise to nth power
+    # Raise to nth power
     elif oper == "5":
         value_one = float(input("\nFirst value:"))
         value_two = float(input("\nPower to raise to:"))
         print(f"The answer is: {value_one ** value_two}")
         break
 
+    # Logarithm
     elif oper == "6":
-        # value_one = float(input("\nFirst value:"))
-        # value_two = float(input("\nSecond value:"))
-        # print(f"The answer is: {value_one / value_two}")
-        print("coming soon...")
+        value_one = float(input("\nEnter value: "))
+        base = float(input("\nBase of the logarith :"))
+        print(f"The answer is: {math.log(value_one, base)}")
         break
-
+    
+    # Sine
     elif oper == "7":
-        # value_one = float(input("\nFirst value:"))
-        # value_two = float(input("\nSecond value:"))
-        # print(f"The answer is: {value_one / value_two}")
-        print("coming soon...")
+        value_one = float(input("\nEnter value: "))
+        print(f"The answer is: {math.sin(value_one)}")
         break
 
+    # Cosine
     elif oper == "8":
-        # value_one = float(input("\nFirst value:"))
-        # value_two = float(input("\nSecond value:"))
-        # print(f"The answer is: {value_one / value_two}")
-        print("coming soon...")
+        value_one = float(input("\nEnter value:"))
+        print(f"The answer is: {math.cos(value_one)}")
         break
 
+    # Tangent
     elif oper == "9":
-        # value_one = float(input("\nFirst value:"))
-        # value_two = float(input("\nSecond value:"))
-        # print(f"The answer is: {value_one / value_two}")
-        print("coming soon...")
+        value_one = float(input("\nEnter value:"))
+        print(f"The answer is: {math.tan(value_one)}")
         break
